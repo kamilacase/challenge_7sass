c4c988271712641b645ae28dd123e46fefaedcd4
Lenormand <xakdxshooter@live.fr>
2018-07-17 16:59:05 +0200

    last

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
-$(document).ready(function(){
-  console.log('hello world');
+$(document).ready(function () {
+  $('main h4:contains("BUY")').addClass("brown");
+  $('main h4:contains("ON SALE")').addClass("yellow");
+  $('main h4:contains("SOLD OUT")').addClass("grey");
+  // $('footer h3').addClass("titleFooter");
 })
